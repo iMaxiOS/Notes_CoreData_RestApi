2b1350bd1d4702a58887b4e38ef06409f4afd200
iMaxiOS <ukr-maksim1@yandex.ru>
2018-07-02 13:52:26 +0300

    first commit

---
+++ NotesCoreData_RestApi/Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NotesCoreData_RestApi
+//
+//  Created by iMaxiOS on 7/2/18.
+//  Copyright © 2018 Maxim Granchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
